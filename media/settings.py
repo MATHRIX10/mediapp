caa356c170eafbb304e4835111d8e447fe94ad6e
rachiderrami <errami.rachid@outlook.com>
2023-01-20 09:25:47 -0500

    adding the user profile model

--- media/settings.py
+++ media/settings.py
@@ -116,6 +116,6 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/mediafiles/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles/')
