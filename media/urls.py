10ac2167f0b9afc748e6cb8a55fef8280a887f01
rachiderrami <errami.rachid@outlook.com>
2023-01-19 23:56:23 -0500

    completting password reset

--- media/urls.py
+++ media/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('login/',views.userLogin,name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name = 'mediapp/logout.html') , name = 'logout'), 
     path('',views.index,name='index'),
-    path('reset',auth_views.PasswordChangeView.as_view(template_name = 'mediapp/reset.html'),name='reset'),
-    path('reset/done/',auth_views.PasswordChangeDoneView.as_view(template_name = 'mediapp/changed.html'),name='password_change_done'),
+    path('change',auth_views.PasswordChangeView.as_view(template_name = 'mediapp/change.html'),name='change'),
+    path('change/done/',auth_views.PasswordChangeDoneView.as_view(template_name = 'mediapp/changed.html'),name='password_change_done'),
+    path('reset/',auth_views.PasswordResetView.as_view(template_name = 'mediapp/reset.html'),name='reset'),
+    path('reset/done',auth_views.PasswordResetDoneView.as_view(template_name = 'mediapp/reset_done.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name = 'mediapp/reset_confirm.html'),name='password_reset_confirm'),
+    path('reset/ended/',auth_views.PasswordResetCompleteView.as_view(template_name = 'mediapp/reset_complete.html'),name = 'password_reset_complete')
+
 ]   
