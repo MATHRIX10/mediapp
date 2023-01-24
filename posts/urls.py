ff7abb4f916cdc79ba2c172d08c17019687bab77
rachiderrami <errami.rachid@outlook.com>
2023-01-23 22:47:06 -0500

    adding like functionality

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('create/',views.post_create, name ='create'),
-    path('feed',views.feed,name='feed')
+    path('feed',views.feed,name='feed'),
+    path('like',views.like_post,name='like'), 
+    
 
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Post 
 from .forms import PostCreationForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404,redirect
+
 # Create your views here.
 
 
@@ -34,3 +36,13 @@ def feed(request) :
     posts = Post.objects.all() 
 
     return render(request,'posts/feed.html',{'posts':posts})
+
+
+def like_post(request) :
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post,id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists() :
+        post.liked_by.remove(request.user)
+    else : 
+        post.liked_by.add(request.user)
+    return redirect('feed')
